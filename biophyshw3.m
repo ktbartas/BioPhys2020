86f6b0293a1e185d7112ab4fc56ec400dfd74e36
ktbartas <70544196+ktbartas@users.noreply.github.com>
2020-10-20 20:53:27 -0700

    fixed my dumbo mistake i think and added plot

--- biophyshw3.m
+++ biophyshw3.m
@@ -1,7 +1,8 @@
 clear; close all; clc;
 syms h(r) ;
-eqn= 0 == 2*diff(h,r,2)/r - 2*diff(h,r)/(r^2) + 8*diff(h,r,3) + 2*r*diff(h,r,4) ;
-Dh = diff(h,r);
-cond = [Dh(5)== 0, Dh(100)==0, h(0)==50];
+eqn= 0 == 2*diff(h,r,2)/r - 2*diff(h,r,1)/(r^2) + 8*diff(h,r,3) + 2*r*diff(h,r,4) ;
+Dh = diff(h,r,1);
+cond = [Dh(5)== 0, Dh(100)==0, h(5)==50, h(100)==0];
 hsolution = dsolve(eqn,cond)
-
+rvals=5:0.1:100;
+ezplot(hsolution,[5,100])
